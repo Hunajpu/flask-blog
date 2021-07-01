969571cf93faa1e3d9af776e2e732be8fe9bd0ac
Hunajpu <irodrigoro@gmail.com>
2021-07-01 00:19:28 +0000

    change on URL

--- endpoint_test.sh
+++ endpoint_test.sh
@@ -7,7 +7,7 @@ endpoints=($endpoints)
 i=0
 for i in {1..6}
 do
-    code=`curl -sI https://rodrigoportfolio.duckdns.org/${endpoints[i]} | head -n 1 | sed -e 's/.*[^0-9]\([0-9]\+\)[^0-9]*$/\1/'`
+    code=`curl -sI http://rodrigoportfolio.duckdns.org/${endpoints[i]} | head -n 1 | sed -e 's/.*[^0-9]\([0-9]\+\)[^0-9]*$/\1/'`
     echo "Endpoint ${endpoints[i]} return code $code"
     ((i++))
 done
