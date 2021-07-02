c3d7853dca116db5a23c23d9ba52c95da6a35670
Rodrigo Luna <irodrigoro@gmail.com>
2021-07-01 17:18:58 -0700

    Adding a SQL database and removing support for sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 import os
 from flask import Flask, render_template, send_from_directory, redirect, url_for, session, request
 from werkzeug.security import check_password_hash, generate_password_hash
-from app.db import get_db
-from . import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from flask_bootstrap import Bootstrap
 from dotenv import load_dotenv
 from datetime import datetime
@@ -16,94 +17,94 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 app = Flask(__name__)
-global admin
-admin = 0
-app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-db.init_app(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
+    user=os.getenv('POSTGRES_USER'),
+    passwd=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('POSTGRES_HOST'),
+    port=5432,
+    table=os.getenv('POSTGRES_DB'))
 
-class PostForm(FlaskForm):
-    body = TextAreaField("What's on your mind?", validators=[DataRequired()])
-    submit = SubmitField('Submit')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 @app.route('/health')
 def health():
-	return "<p>Hello</p>", 200
+	return "<p>Works</p>", 200
 
 @app.route('/logout')
 def logout():
-    global admin
-    admin = 0
-    return render_template('index.html', title="Rodrigo Luna", session_type=admin, url=os.getenv("URL")), 200 
+    return render_template('index.html', title="Rodrigo Luna", session_type=0, url=os.getenv("URL")), 200 
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
     error = 'e'
-    global admin
-    admin = 0
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
         error = None
-        db = get_db()
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        user = UserModel.query.filter_by(username=username).first()
 
-        if user is None:
+        if user is username:
             error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
+        elif not check_password_hash(user.password, password):
             error = 'Incorrect password.'
 
         if error is None:
-            return render_template('index.html', title="Rodrigo Luna", session_type=admin, url=os.getenv("URL")), 200 
+            return render_template('index.html', title="Rodrigo Luna", session_type=0, url=os.getenv("URL")), 200 
         else:
-            return render_template('login.html', title="Login", session_type=admin, err=error), 418
+            return render_template('login.html', title="Login", session_type=0, err=error), 418
 
-    return render_template('login.html', title="Login", session_type=admin, err=error)
+    return render_template('login.html', title="Login", session_type=0, err=error)
 
 @app.route('/register', methods=('GET', 'POST'))
 def register():
     error = 'e'
-    global admin
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
 
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
-            return render_template('register.html',  title="Register", session_type=admin, err="Register successful")
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+            return render_template('register.html',  title="Register", session_type=0, err="Register successful")
         else:
-            return render_template('register.html',  title="Register", session_type=admin, err=error), 418
+            return render_template('register.html',  title="Register", session_type=0, err=error), 418
 
-    return render_template('register.html', title="Register", session_type=admin, err=error)
+    return render_template('register.html', title="Register", session_type=0, err=error)
 
 @app.route('/')
 def index():
-    global admin
-    return render_template('index.html', title="Rodrigo Luna", session_type=admin, url=os.getenv("URL"))
+    return render_template('index.html', title="Rodrigo Luna", session_type=0, url=os.getenv("URL"))
 
 
 @app.route('/contact')
 def contact():
-    global admin
-    return render_template('contacts.html', title="Contact", session_type=admin, url=os.getenv("URL"))
+    return render_template('contacts.html', title="Contact", session_type=0, url=os.getenv("URL"))
 
 @app.route('/form',methods=["POST"])
 def form():
@@ -120,16 +121,11 @@ def form():
 
 @app.route('/blog', methods=['GET', 'POST'])
 def blog():
-    form = PostForm()
-    if form.validate_on_submit():
-        session['post'] = form.body.data
-        return redirect(url_for('blog'))
-    return render_template('blog.html', title="Blog", url=os.getenv("URL"), form=form, post=session.get('post'))
+    return "Blog"
 
 
 @app.route('/projects')
 def projects():
-    global admin
     # Hardcoded projects names
     robotics_projects = ['Sumo Robot/sumo.jpg', 'Line Following Robot/line_follower.png', 'Soccer Robot/soccer_robot.jpeg', 'Fire Extinguishing Robot/fire_robot.jpg']
     electronics_projects = ['Cell Phone Detector/Cell-phone-detector.jpg', 'Mobile Jammer Circuit/Mobile-Jammer.jpg']
@@ -143,4 +139,4 @@ def projects():
     else:
 	    page = 1
 	
-    return render_template('projects.html', title="Projects", session_type=admin, url=os.getenv("URL"), projects=projects_names,pag = page)
+    return render_template('projects.html', title="Projects", session_type=0, url=os.getenv("URL"), projects=projects_names,pag = page)
